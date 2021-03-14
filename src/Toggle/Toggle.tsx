fd8fe371ff67f688d7aa478470de25b9b8cb279c
Nathan Innes <nathaninnes001@gmail.com>
2021-03-14 02:19:16 +0000

    fix: partial eslint failures on proptypes

--- src/Toggle/Toggle.tsx
+++ src/Toggle/Toggle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export type ToggleProps = { value: boolean, onToggle: (value: boolean) => void, dark?: boolean; };
 
-const Toggle: React.FC<ToggleProps> = ({ value, onToggle, dark = true }) => (
+const Toggle: React.FC<ToggleProps> = ({ value, onToggle, dark = true }: ToggleProps) => (
     <div
         onClick={() => onToggle(!value)}
         className={`w-12 h-6 px-0.5 ${dark ? 'bg-blue-darker' : 'bg-gray-200'} rounded-full shadow-inner-sm flex flex-row items-center`}
