b1c0a319f7c18260879713032f96b3768822e34f
Florian Scheuner <github@florian-scheuner.de>
2021-04-17 08:02:22 +0200

    fix: cursor: pointer (#6)

--- src/Toggle/Toggle.tsx
+++ src/Toggle/Toggle.tsx
@@ -6,7 +6,7 @@ const Toggle: React.FC<ToggleProps> = ({ value, onToggle, dark = true }) => (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
     <div
         onClick={() => onToggle(!value)}
-        className={`w-12 h-6 px-0.5 ${dark ? 'bg-blue-darker' : 'bg-gray-200'} rounded-full shadow-inner-sm flex flex-row items-center`}
+        className={`w-12 h-6 px-0.5 ${dark ? 'bg-blue-darker' : 'bg-gray-200'} rounded-full shadow-inner-sm flex flex-row items-center cursor-pointer`}
     >
         <div
             className={
