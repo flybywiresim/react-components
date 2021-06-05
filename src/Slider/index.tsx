f988793e76fb375d8e6732b4cf34a0070964e91e
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2021-06-05 19:18:37 +0530

    remove pre defined width from slider 🧺

--- src/Slider/index.tsx
+++ src/Slider/index.tsx
@@ -23,7 +23,7 @@ export const Slider: React.FC<SliderProps> = ({ className, onInput, value, dark 
             type="range"
             min="1"
             max="100"
-            className={`${dark ? 'slider' : 'slider-light'} h-1 bg-blue-darker w-60 h-1 pb-1 pt-0 rounded-full shadow-inner-sm ${className}`}
+            className={`${dark ? 'slider' : 'slider-light'} h-1 bg-blue-darker h-1 pb-1 pt-0 rounded-full shadow-inner-sm ${className}`}
         />
     );
 };
