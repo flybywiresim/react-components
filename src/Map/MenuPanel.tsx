eee015ddada5106a35aa784c4090c66937ebc1d1
Nathan Innes <nathaninnes001@gmail.com>
2021-06-10 07:58:53 +0100

    feat(map menu): click to close menu (#9)
    * feat(map menu): click to close menu
    
    * style(map menu): ui improvements
    
    * fix: add color change when details are shown
    
    Co-authored-by: benjozork <benjamin.dupont.984@gmail.com>

--- src/Map/MenuPanel.tsx
+++ src/Map/MenuPanel.tsx
@@ -111,104 +111,107 @@ const MenuPanel = (props: MenuPanelProps): JSX.Element => {
     }
 
     return (
-        <div id="menu-panel" className="leaflet-top leaflet-left flex flex-col">
-            <div className="w-full z-50 flex flex-row items-center h-11 px-3 py-2 bg-gray-50 border-2 border-teal-300 rounded-md shadow-md">
-                <button
-                    type="button"
-                    className="menu-button w-8 border-0 pointer focus:outline-none"
-                    aria-label="Menu"
-                    onClick={() => setShowDetails(!showDetails)}
-                >
-                    <IconChevronDown className={`text-gray-500 transform transition-transform ${showDetails ? 'rotate-180' : ''}`} />
-                </button>
-                <input
-                    type="text"
-                    aria-label="Search Field"
-                    className="text-base bg-gray-50 placeholder-gray-500 text-gray-600 focus:outline-none"
-                    list="autocomplete"
-                    placeholder="Flight Number"
-                    onChange={(event) => {
-                        setSearchValue(event.target.value.toString());
-                        handleSearch(false, event.target.value.toString());
-                    }}
-                    onKeyPress={(event) => (event.key === 'Enter' ? handleSearch() : {})}
-                    onFocus={(event) => event.target.select()}
-                    onBlur={() => handleSearch()}
-                />
-                <button
-                    type="submit"
-                    aria-label="Search"
-                    onClick={() => handleSearch()}
-                    className="search-button ml-auto border-0 pointer"
-                >
-                    <IconSearch className="text-gray-500" />
-                </button>
-                <datalist id="autocomplete">
-                    {
-                        autocompleteList.map((connection) => (!searchValue || connection.flight.startsWith(searchValue)
+        <>
+            <div id="menu-panel" className="leaflet-top leaflet-left flex flex-col shadow-lg">
+                <div className={`w-full z-50 flex flex-row items-center h-11 px-3 py-2 ${showDetails ? 'bg-gray-50' : 'bg-white'} border-2 border-gray-100 rounded-md`}>
+                    <button
+                        type="button"
+                        className="menu-button w-8 border-0 pointer focus:outline-none"
+                        aria-label="Menu"
+                        onClick={() => setShowDetails(!showDetails)}
+                    >
+                        <IconChevronDown className={`text-gray-500 transform transition-transform ${showDetails ? 'rotate-180' : ''}`} />
+                    </button>
+                    <input
+                        type="text"
+                        aria-label="Search Field"
+                        className={`text-base ${showDetails ? 'bg-gray-50' : 'bg-white'} placeholder-gray-500 text-gray-600 focus:outline-none`}
+                        list="autocomplete"
+                        placeholder="Flight Number"
+                        onChange={(event) => {
+                            setSearchValue(event.target.value.toString());
+                            handleSearch(false, event.target.value.toString());
+                        }}
+                        onKeyPress={(event) => (event.key === 'Enter' ? handleSearch() : {})}
+                        onFocus={(event) => event.target.select()}
+                        onBlur={() => handleSearch()}
+                    />
+                    <button
+                        type="submit"
+                        aria-label="Search"
+                        onClick={() => handleSearch()}
+                        className="search-button ml-auto border-0 pointer"
+                    >
+                        <IconSearch className="text-gray-500" />
+                    </button>
+                    <datalist id="autocomplete">
+                        {
+                            autocompleteList.map((connection) => (!searchValue || connection.flight.startsWith(searchValue)
                             // eslint-disable-next-line jsx-a11y/control-has-associated-label
-                            ? <option key={connection.id} value={connection.flight} /> : <></>))
+                                ? <option key={connection.id} value={connection.flight} /> : <></>))
+                        }
+                    </datalist>
+                </div>
+                <div className="divider" hidden={!showDetails} />
+                <div className="detail-area bg-white px-4 py-4 text-gray-50 divide-y divide-gray-300 -mt-1.5 rounded-b-md shadow-lg" hidden={!showDetails}>
+                    {
+                        (props.weatherOpacity !== undefined && props.onWeatherOpacityChange)
+                            ? (
+                                <>
+                                    <h1 className="text-base text-gray-700 font-semibold">Weather opacity</h1>
+
+                                    <Slider
+                                        className="w-full"
+                                        dark={false}
+                                        value={100 * props.weatherOpacity}
+                                        onInput={(value) => props.onWeatherOpacityChange && props.onWeatherOpacityChange(Number(value) / 100)}
+                                    />
+                                </>
+                            ) : <></>
                     }
-                </datalist>
-            </div>
-            <div className="divider" hidden={!showDetails} />
-            <div className="detail-area bg-gray-100 px-4 py-4 text-gray-50 divide-y divide-gray-300 -mt-1.5 rounded-b-md shadow-lg" hidden={!showDetails}>
-                {
-                    (props.weatherOpacity !== undefined && props.onWeatherOpacityChange)
-                        ? (
-                            <>
-                                <h1 className="text-base text-gray-700 font-semibold">Weather opacity</h1>
+                    {
+                        (props.activeTileSet && props.availableTileSets && props.onTileSetChange)
+                            ? (
+                                <div className="mt-3 pt-2">
+                                    <h1 className="text-base text-gray-700 font-semibold mb-1">Theme</h1>
 
-                                <Slider
-                                    className="w-full"
-                                    dark={false}
-                                    value={100 * props.weatherOpacity}
-                                    onInput={(value) => props.onWeatherOpacityChange && props.onWeatherOpacityChange(Number(value) / 100)}
-                                />
-                            </>
-                        ) : <></>
-                }
-                {
-                    (props.activeTileSet && props.availableTileSets && props.onTileSetChange)
-                        ? (
-                            <div className="mt-3 pt-2">
-                                <h1 className="text-base text-gray-700 font-semibold mb-1">Theme</h1>
-
-                                <div onChange={handleTileSelect} className="flex flex-row space-x-4">
-                                    {
-                                        props.availableTileSets.map((tileSet) => (
+                                    <div onChange={handleTileSelect} className="flex flex-row space-x-4">
+                                        {
+                                            props.availableTileSets.map((tileSet) => (
                                             // eslint-disable-next-line jsx-a11y/label-has-associated-control
-                                            <label className="tileset-select flex-grow" key={tileSet.value}>
-                                                <input
-                                                    type="radio"
-                                                    name="tileset"
-                                                    value={tileSet.value}
-                                                    defaultChecked={props.activeTileSet && tileSet.value === props.activeTileSet.value}
-                                                />
-                                                <img src={tileSet.previewImageUrl} alt={tileSet.name} className="w-full rounded-sm shadow-md" />
-                                            </label>
-                                        ))
-                                    }
+                                                <label className="tileset-select flex-grow" key={tileSet.value}>
+                                                    <input
+                                                        type="radio"
+                                                        name="tileset"
+                                                        value={tileSet.value}
+                                                        defaultChecked={props.activeTileSet && tileSet.value === props.activeTileSet.value}
+                                                    />
+                                                    <img src={tileSet.previewImageUrl} alt={tileSet.name} className="w-full rounded-sm shadow-md" />
+                                                </label>
+                                            ))
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-                        ) : <></>
-                }
-                {
-                    props.onShowOthersChange
-                        ? (
-                            <div className="flex flex-row justify-between items-center mt-3.5 pt-3 pb-3">
-                                <h1 className="text-base text-gray-700 font-semibold">Show others</h1>
+                            ) : <></>
+                    }
+                    {
+                        props.onShowOthersChange
+                            ? (
+                                <div className="flex flex-row justify-between items-center mt-3.5 pt-3 pb-3">
+                                    <h1 className="text-base text-gray-700 font-semibold">Show others</h1>
 
-                                <Toggle dark={false} value={props.showOthers} onToggle={props.onShowOthersChange ?? (() => {})} />
-                            </div>
-                        ) : <></>
-                }
-                <div className="flex flex-row justify-between items-center pt-3 -mb-1.5">
-                    <h1 className="text-base uppercase text-gray-700 font-semibold">Active Flights</h1>
-                    <h1 className="text-4xl font-semibold text-teal-light-contrast">{totalFlights.toString()}</h1>
+                                    <Toggle dark={false} value={props.showOthers} onToggle={props.onShowOthersChange ?? (() => {})} />
+                                </div>
+                            ) : <></>
+                    }
+                    <div className="flex flex-row justify-between items-center pt-3 -mb-1.5">
+                        <h1 className="text-base uppercase text-gray-700 font-semibold">Active Flights</h1>
+                        <h1 className="text-4xl font-semibold text-teal-light-contrast">{totalFlights.toString()}</h1>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={`h-full w-full ${showDetails ? '' : 'hidden'}`} onClick={() => setShowDetails(!showDetails)} />
+        </>
     );
 };
 
